9f913a6c65aca98920949e8b25c62c760de7762f
Mark Erwin C. Villarina <me.villarina@live.com>
2021-03-23 13:25:40 +0800

    Housekeeping

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace CodeAddict
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Code Addict!");
         }
     }
 }
